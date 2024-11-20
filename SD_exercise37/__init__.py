913912adc561bb5acc0e05f8894f7ede0b93dc3b
SNEHILA TIWARI <38729246+SnehaTri@users.noreply.github.com>
2024-11-20 13:31:57 +0100

    second commit

---
+++ SD_exercise37/__init__.py
@@ -0,0 +1,19 @@
+def add(a: int, b: int) -> int:
+    """
+    Adds two integers and returns the result.
+
+    :param a: First integer
+    :param b: Second integer
+    :return: The sum of a and b
+    """
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    """
+    Subtracts second integer from the first and returns the result.
+
+    :param a: First integer
+    :param b: Second integer
+    :return: The difference between a and b
+    """
+    return a - b
